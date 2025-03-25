92f1c10a6d16185dc25d1fe3bfd796d7bcd89864
selenemoon78 <asmaa.bahammou@edu.esiee-it.fr>
2025-03-25 09:33:43 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,26 @@
-DROP TABLE IF EXISTS clients;
-CREATE TABLE clients (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    nom TEXT NOT NULL,
-    prenom TEXT NOT NULL,
-    adresse TEXT NOT NULL
+CREATE TABLE Livres (
+    ID_livre INT PRIMARY KEY,
+    Titre VARCHAR(255),
+    Auteur VARCHAR(255),
+    Annee_publication INT,
+    Quantite INT
+);
+
+CREATE TABLE Utilisateurs (
+    ID_utilisateur INT PRIMARY KEY,
+    Nom VARCHAR(255),
+    Prenom VARCHAR(255),
+    Email VARCHAR(255),
+    Date_inscription TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Emprunts (
+    ID_emprunt INT PRIMARY KEY,
+    ID_utilisateur INT,
+    ID_livre INT,
+    Date_emprunt DATE,
+    Date_retour_prevue DATE,
+    Date_retour_effective DATE,
+    FOREIGN KEY (ID_utilisateur) REFERENCES Utilisateurs(ID_utilisateur),
+    FOREIGN KEY (ID_livre) REFERENCES Livres(ID_livre)
 );
